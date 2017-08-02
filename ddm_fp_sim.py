f7ba4047679366c6fc6fc953822606c68051d0bb
Spstolar <spstolar@math.uh.edu>
2017-08-02 18:51:56 -0500

    Cleans up code and adds a calculation for information gained by reactions to a decision (log R+ / R- in the clique notes).

--- ddm_fp_sim.py
+++ ddm_fp_sim.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 # model params
 g = 1  # drift
 s = 1  # variance
-hp = 4  # upper threshold
-hm = -1  # lower threshold
+hp = 3  # upper threshold
+hm = -3.0  # lower threshold
 L = hp-hm  # length of the domain
 
 # discretization parameters
@@ -27,14 +27,15 @@ Pm = np.copy(Pp)
 surv_prob_P = np.zeros(Nt)
 surv_prob_P[0] = 1 
 surv_prob_M = np.copy(surv_prob_P)
+pos_surv_pos = np.zeros(Nt)  # probability mass above zero but not escaping given H^+
+pos_surv_neg = np.zeros(Nt)  # ^^ for H^-
 
 # Crank-Nicolson discretization of diffusion
 scaling_term = (s * s) / (2 * dx * dx)
-D = scaling_term * (np.eye(Nx,k=1) -2 * np.eye(Nx,k=0) + np.eye(Nx,k=-1))  # scaling term times matrix with diagonal -2 and off diagonals 1
+D = scaling_term * (np.eye(Nx,k=1) -2 * np.eye(Nx,k=0) + np.eye(Nx,k=-1))  # scalar * (diagonal -2, off diagonals 1)
 
 # discretization of advection for plus and minus drift
 Ap = (-g / dx) * (np.eye(Nx,k=1) - np.eye(Nx,k=0))
-#Am = (-g / dx) * (np.eye(Nx,k=-1) - np.eye(Nx,k=0))
 Am = -np.copy(Ap)
 
 # create Crank-Nicolson step matrices
@@ -44,28 +45,36 @@ Mm = np.dot(diff_inverse, (np.eye(Nx) + dt*Am + (dt / 2)*D))
 
 # run CN steps
 for j in range(1,Nt):  # j from 1 to Nt-1
-    Pp[:,j] = np.dot(Mp, Pp[:,j-1])
-    Pm[:,j] = np.dot(Mm, Pm[:,j-1])  # probability
-    surv_prob_P[j] = dx * np.sum(Pp[:,j])
-    surv_prob_M[j] = dx * np.sum(Pm[:,j])  # survival
+    Pp[:, j] = np.dot(Mp, Pp[:, j-1])
+    Pm[:, j] = np.dot(Mm, Pm[:, j-1])  # probability
+    surv_prob_P[j] = dx * np.sum(Pp[:, j])
+    surv_prob_M[j] = dx * np.sum(Pm[:, j])  # survival
+    right_pos = dx * np.sum(Pp[zero_ind:, j])
+    right_neg = dx * np.sum(Pm[zero_ind:, j])
+    pos_surv_pos[j] = right_pos / surv_prob_P[j]
+    pos_surv_neg[j] = right_pos / surv_prob_M[j]
+
 
 plt.figure(1)
-plt.pcolormesh(xvec, tvec, Pp.T, cmap='hot', vmin=0, vmax=0.2) #pcolormesh is much faster than pcolor in python for large arrays
+plt.pcolormesh(xvec, tvec, Pp.T, cmap='hot', vmin=0, vmax=0.2)  # pcolormesh is much faster than pcolor for large arrays
 plt.axis('off')
 # shading flat  # flat is actually the default shading option
 
-
-plt.figure(2) 
+plt.figure(2)
 plt.pcolormesh(xvec, tvec, Pm.T, cmap='hot', vmin=0, vmax=0.2)
 plt.axis('off')
 
 
 plt.figure(3)
-plt.plot(tvec, surv_prob_P, color='red', linewidth=8)
-plt.plot(tvec, surv_prob_M, color='blue', linewidth=8)
-u = np.log( surv_prob_P / surv_prob_M)
-plt.plot(tvec, u, 'k--', linewidth=8)
-plt.axis([0, T, 0, 3])
+plt.plot(tvec, surv_prob_P, color='red', linewidth=8)  # survival prob H^+
+plt.plot(tvec, surv_prob_M, color='blue', linewidth=8)  # survival prob H^-
+u = np.log(surv_prob_P / surv_prob_M)
+max_info = np.amax(u)
+y_max = np.amax((max_info, 1)) + 0.5
+plt.plot(tvec, u, 'k--', linewidth=8)  # non-decision information
+plt.axis([0, T, 0, max_info + y_max])
+r_prob_ratio = np.log(pos_surv_pos / pos_surv_neg)  # log R_+ / R_-  clique info
+plt.plot(tvec, r_prob_ratio, color='orange', linewidth=4)
 #set(gca,'xtick',[])
 #set(gca,'ytick',[0:3])
 #set(gca,'fontsize',30)
