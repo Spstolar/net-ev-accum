57a9700e91327018da948d5db0aac456672c4c2f
Spstolar <spstolar@math.uh.edu>
2017-10-23 00:54:40 -0500

    Fixes LLR store and runs correctness estimates.

--- Code/GeneralNetworks/directed_line.py
+++ Code/GeneralNetworks/directed_line.py
@@ -9,8 +9,9 @@ mean1 = 0.1
 mean2 = -0.1
 var1 = 1
 var2 = 1
-bdy_plus = 3
-bdy_minus = -3
+alpha = 0.8
+bdy_plus = np.log(alpha / (1 - alpha))
+bdy_minus = - bdy_plus
 
 
 class Dist:
@@ -39,10 +40,12 @@ def compute_llr(x_array, dist1, dist2):
     return np.log(dist1(x_array)/dist2(x_array))
 
 # Simulation Parameters
-num_agents = 3
-num_sims = 1
-time_limit = 300
-bump = bdy_plus * np.ones(num_agents)  # This is how much an agent alters LLR when previous agent decides.
+num_agents = 10
+num_sims = int(1e4)
+time_limit = int(1e3)
+bump = np.zeros(num_agents)  # This is how much an agent alters LLR when previous agent decides.
+for a in range(0,num_agents):
+    bump[a] = (1.1 ** a) * bdy_plus
 llr_store = np.zeros((num_agents, time_limit))
 final_time = 0
 correct = 0  # Number of correct decisions by the end agent.
@@ -68,8 +71,6 @@ for s in range(num_sims):
     while decided == 0 and t < time_limit:
         obs = np.sqrt(var1) * np.random.randn(num_agents) + mean1  # make observations
         llr += compute_llr(obs, pos.prob, neg.prob) * still_accumulating  # incorporate obs for active agents
-        if s == 0:
-            llr_store[:, t] = llr  # for visualization
 
         for a in np.arange(num_agents):
             if still_accumulating[a] == 1:  # skip all of this if the agent is done
@@ -77,31 +78,38 @@ for s in range(num_sims):
                 decisions[a] = check_decision(llr[a], bdy_minus, bdy_plus)
 
                 if decisions[a] != 0:  # check if the observation caused a decision
+                    # print str(a) + ' decided'
                     next_fire = 1
                     still_accumulating[a] = 0
                 else:
                     next_fire = 0
                 
                 if fire == 1 and still_accumulating[a] == 1:  # if neighbor decided, then bump
+                    # print 'fire'
                     llr[a] += decisions[a-1] * bump[a]
                     decisions[a] = check_decision(llr[a], bdy_minus, bdy_plus)
                     if decisions[a] != 0:  # check if the bump caused a decision
                         next_fire = 1
                         still_accumulating[a] = 0
 
+        if s == 0:
+            llr_store[:, t] = llr  # for visualization
+
         if decisions[-1] == 1:  # if the last agent decides, log and exit
             correct += 1
             decided = 1
             if s == 0:
-                final_time = t + 1
+                final_time = t
         elif decisions[-1] == -1:
             decided = 1
             if s == 0:
-                final_time = t + 1
+                final_time = t
         t += 1
 
     if s == 0:
         print decisions
+
+print correct / float(num_sims)
                 
 for a in range(num_agents):
     plt.plot(llr_store[a,:final_time+1], label=str(a+1))
